d088c0aaec98f67ea1c1b664a2e242ee4daef177
tmatsuzawa2 <tmatsuzawa@wisc.edu>
2021-04-27 19:51:51 -0500

    Added links for plays and articles

--- src/components/Carousel/right.tsx
+++ src/components/Carousel/right.tsx
@@ -5,6 +5,7 @@ import { playText } from "../../text/carouselText";
 import { photoUrl } from "../../libs/security";
 import PlayCard from "../Cards/playCard";
 import { useHistory } from "react-router";
+import { playList } from "../../libs/footer";
 
 interface RightCarouselProps {
   content: { name: string; year: string }[];
@@ -65,8 +66,8 @@ const RightCarousel: FC<RightCarouselProps> = (props) => {
             <PlayCard
               key={item.name}
               onClick={() => {
-                if (item.name.includes("玫瑰"))
-                  history.push("/play/红玫瑰与白玫瑰");
+                let link = playList.find(x => x.name == item.name)?.url;
+                window.location.href = (link !== undefined) ? link : "https://sayingtheatre.com/";
               }}
               className="carousel-card"
               name={`${item.name}`}
